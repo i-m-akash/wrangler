275d8773d34162cfc86a87afd74a3e8f43f0dac6
Akash kumar <109230614+i-m-akash@users.noreply.github.com>
2025-04-12 23:08:17 +0530

    Update TokenType.java

--- wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
+++ wrangler-api/src/main/java/io/cdap/wrangler/api/parser/TokenType.java
@@ -1,63 +1,234 @@
+package io.cdap.wrangler.parser;
 
+import io.cdap.wrangler.api.LazyNumber;
+import io.cdap.wrangler.api.RecipeSymbol;
+import io.cdap.wrangler.api.SourceInfo;
+import io.cdap.wrangler.api.Triplet;
+import io.cdap.wrangler.api.parser.Bool;
+import io.cdap.wrangler.api.parser.BoolList;
+import io.cdap.wrangler.api.parser.ByteSize;
+import io.cdap.wrangler.api.parser.ColumnName;
+import io.cdap.wrangler.api.parser.ColumnNameList;
+import io.cdap.wrangler.api.parser.DirectiveName;
+import io.cdap.wrangler.api.parser.Expression;
+import io.cdap.wrangler.api.parser.Identifier;
+import io.cdap.wrangler.api.parser.Numeric;
+import io.cdap.wrangler.api.parser.NumericList;
+import io.cdap.wrangler.api.parser.Properties;
+import io.cdap.wrangler.api.parser.Ranges;
+import io.cdap.wrangler.api.parser.Text;
+import io.cdap.wrangler.api.parser.TextList;
+import io.cdap.wrangler.api.parser.TimeDuration;
+import io.cdap.wrangler.api.parser.Token;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.Interval;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
-/*
- * Copyright © 2017-2019 Cask Data, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This class <code>RecipeVisitor</code> implements the visitor pattern
+ * used during traversal of the AST tree. The <code>ParserTree#Walker</code>
+ * invokes appropriate methods as callbacks with information about the node.
  */
+public final class RecipeVisitor extends DirectivesBaseVisitor<RecipeSymbol.Builder> {
+    private RecipeSymbol.Builder builder = new RecipeSymbol.Builder();
 
- package io.cdap.wrangler.api.parser;
+    /**
+     * Returns a <code>RecipeSymbol</code> for the recipe being parsed.
+     */
+    public RecipeSymbol getCompiledUnit() {
+        return builder.build();
+    }
 
- import com.google.gson.JsonElement;
- import io.cdap.wrangler.api.annotations.PublicEvolving;
- 
- import java.io.Serializable;
- 
- /**
-  * The Token class represents the object that contains the value and type of
-  * the token as parsed by the parser of the grammar defined for recipe.
-  *
-  * <p>This class provides methods for retrieving the wrapped value of token parsed
-  * as well as the type of token the implementation of this interface represents.</p>
-  *
-  * <p>It also provides method for providing the {@code JsonElement} of implementation
-  * of this interface.</p>
-  */
- @PublicEvolving
- public interface Token extends Serializable {
-   
-   /**
-    * Returns the {@code value} of the object wrapped by the
-    * implementation of this interface.
-    *
-    * @return {@code value} wrapped by the implementation of this interface.
-    */
-   Object value();
- 
-   /**
-    * Returns the {@code TokenType} of the object represented by the
-    * implementation of this interface.
-    *
-    * @return {@code TokenType} of the implementation object.
-    */
-   TokenType type();
- 
-   /**
-    * The class implementing this interface will return the {@code JsonElement}
-    * instance including the values of the object.
-    *
-    * @return {@code JsonElement} object containing members of the implementing class.
-    */
-   JsonElement toJson();
- }
- 
+    @Override
+    public RecipeSymbol.Builder visitDirective(DirectivesParser.DirectiveContext ctx) {
+        builder.createTokenGroup(getOriginalSource(ctx));
+        return super.visitDirective(ctx);
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitIdentifier(DirectivesParser.IdentifierContext ctx) {
+        builder.addToken(new Identifier(ctx.Identifier().getText()));
+        return super.visitIdentifier(ctx);
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitPropertyList(DirectivesParser.PropertyListContext ctx) {
+        Map<String, Token> props = new HashMap<>();
+        for (DirectivesParser.PropertyContext property : ctx.property()) {
+            String identifier = property.Identifier().getText();
+            Token token = parseProperty(property);
+            props.put(identifier, token);
+        }
+        builder.addToken(new Properties(props));
+        return builder;
+    }
+
+    private Token parseProperty(DirectivesParser.PropertyContext property) {
+        if (property.number() != null) {
+            return new Numeric(new LazyNumber(property.number().getText()));
+        } else if (property.bool() != null) {
+            return new Bool(Boolean.valueOf(property.bool().getText()));
+        } else {
+            String text = property.text().getText();
+            return new Text(text.substring(1, text.length() - 1));
+        }
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitPragmaLoadDirective(DirectivesParser.PragmaLoadDirectiveContext ctx) {
+        for (TerminalNode identifier : ctx.identifierList().Identifier()) {
+            builder.addLoadableDirective(identifier.getText());
+        }
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitPragmaVersion(DirectivesParser.PragmaVersionContext ctx) {
+        builder.addVersion(ctx.Number().getText());
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitNumberRanges(DirectivesParser.NumberRangesContext ctx) {
+        List<Triplet<Numeric, Numeric, String>> output = new ArrayList<>();
+        for (DirectivesParser.NumberRangeContext range : ctx.numberRange()) {
+            List<TerminalNode> numbers = range.Number();
+            String text = range.value().getText();
+            if (text.startsWith("'") && text.endsWith("'")) {
+                text = text.substring(1, text.length() - 1);
+            }
+            output.add(new Triplet<>( 
+                new Numeric(new LazyNumber(numbers.get(0).getText())),
+                new Numeric(new LazyNumber(numbers.get(1).getText())),
+                text
+            ));
+        }
+        builder.addToken(new Ranges(output));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitEcommand(DirectivesParser.EcommandContext ctx) {
+        builder.addToken(new DirectiveName(ctx.Identifier().getText()));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitColumn(DirectivesParser.ColumnContext ctx) {
+        builder.addToken(new ColumnName(ctx.Column().getText().substring(1)));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitText(DirectivesParser.TextContext ctx) {
+        String value = ctx.String().getText();
+        builder.addToken(new Text(value.substring(1, value.length() - 1)));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitNumber(DirectivesParser.NumberContext ctx) {
+        LazyNumber number = new LazyNumber(ctx.Number().getText());
+        builder.addToken(new Numeric(number));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitBool(DirectivesParser.BoolContext ctx) {
+        builder.addToken(new Bool(Boolean.valueOf(ctx.Bool().getText())));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitCondition(DirectivesParser.ConditionContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < ctx.getChildCount() - 1; ++i) {
+            sb.append(ctx.getChild(i).getText()).append(" ");
+        }
+        builder.addToken(new Expression(sb.toString()));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitCommand(DirectivesParser.CommandContext ctx) {
+        builder.addToken(new DirectiveName(ctx.Identifier().getText()));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitColList(DirectivesParser.ColListContext ctx) {
+        List<String> names = new ArrayList<>();
+        for (TerminalNode column : ctx.Column()) {
+            names.add(column.getText().substring(1));
+        }
+        builder.addToken(new ColumnNameList(names));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitNumberList(DirectivesParser.NumberListContext ctx) {
+        List<LazyNumber> numerics = new ArrayList<>();
+        for (TerminalNode number : ctx.Number()) {
+            numerics.add(new LazyNumber(number.getText()));
+        }
+        builder.addToken(new NumericList(numerics));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitBoolList(DirectivesParser.BoolListContext ctx) {
+        List<Boolean> booleans = new ArrayList<>();
+        for (TerminalNode bool : ctx.Bool()) {
+            booleans.add(Boolean.parseBoolean(bool.getText()));
+        }
+        builder.addToken(new BoolList(booleans));
+        return builder;
+    }
+
+    @Override
+    public RecipeSymbol.Builder visitStringList(DirectivesParser.StringListContext ctx) {
+    List<TerminalNode> strings = ctx.String();
+    List<String> strs = new ArrayList<>();
+    for (TerminalNode string : strings) {
+      String text = string.getText();
+      strs.add(text.substring(1, text.length() - 1));
+    }
+    builder.addToken(new TextList(strs));
+    return builder;
+  }
+
+  @Override
+public RecipeSymbol.Builder visitValue(DirectivesParser.ValueContext ctx) {
+    String text = ctx.getText();
+
+    if (text.matches("(?i)[0-9]+(\\.[0-9]+)?(KB|MB|GB|TB)")) {
+        builder.addToken(new ByteSize(text));
+    } else if (text.matches("[0-9]+(\\.[0-9]+)?(ms|s|m|h)")) {
+        builder.addToken(new TimeDuration(text));
+    } else {
+        // fallback case: treat as plain text (optional)
+        builder.addToken(new Text(text));
+    }
+
+    return builder;
+}
+
+
+
+    private SourceInfo getOriginalSource(ParserRuleContext ctx) {
+        int a = ctx.getStart().getStartIndex();
+        int b = ctx.getStop().getStopIndex();
+        Interval interval = new Interval(a, b);
+        String text = ctx.start.getInputStream().getText(interval);
+        int lineno = ctx.getStart().getLine();
+        int column = ctx.getStart().getCharPositionInLine();
+        return new SourceInfo(lineno, column, text);
+    }
+}
+
